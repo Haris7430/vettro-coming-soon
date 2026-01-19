be3a0457cf7de39a3306f60a0236ca1d293ce35a
haris <mon986344@gmail.com>
2026-01-20 00:18:58 +0530

    app routes updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { ComingSoonComponent } from './coming-soon/coming-soon';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+ 
+  { 
+    path: '', 
+    component: ComingSoonComponent 
+  },
+
+  
+  { 
+    path: '**', 
+    redirectTo: '', 
+    pathMatch: 'full' 
+  }
+];
